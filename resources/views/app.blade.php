c467e55d75df1a88f4cbe08268c5189ff2192ba2
joseayalaa18 <jose18ayala05@gmail.com>
2026-06-14 00:17:45 -0300

    Logo Tipo de Cronos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/webp" href="{{ asset('imagenes/logoTipoCronosNotes2.png') }}">
+
         <!-- Fonts & Icons -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
